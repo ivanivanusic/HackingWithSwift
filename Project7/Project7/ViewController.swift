bc6e0bdf69c16aaa5daaba66eadf879f88da4400
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-08-25 23:12:41 +0200

    Project 7 - Done

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var petitions = [Petition]()
+    var allPetitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let urlString: String
-
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(creditsTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
+        
         if navigationController?.tabBarItem.tag == 0 {
             // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -38,18 +41,19 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
+            allPetitions = jsonPetitions.results
+            filteredPetitions = allPetitions
             tableView.reloadData()
         }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -57,7 +61,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
     
@@ -65,6 +69,52 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    @objc func creditsTapped() {
+        let vc = UIAlertController(title: "Credits", message: "We The People API of the Whitehouse", preferredStyle: .alert)
+        vc.addAction(UIAlertAction(title: "Exit", style: .default, handler: nil))
+        
+        present(vc, animated: true)
+    }
+    
+    @objc func filterTapped() {
+        let ac = UIAlertController(title: "Enter filter", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Enter", style: .default) {
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.enter(answer)
+        }
+        
+        let clearFilters = UIAlertAction(title: "Clear filter", style: .default, handler: clearFilter)
+        
+        ac.addAction(submitAction)
+        ac.addAction(clearFilters)
+        present(ac, animated: true)
+    }
+    
+    func enter(_ filter: String) {
+        filteredPetitions.removeAll(keepingCapacity: false)
+        
+        if filter == "" {
+            filteredPetitions = allPetitions
+        } else {
+            for petition in allPetitions {
+                if petition.body.contains(filter) || petition.title.contains(filter) {
+                    filteredPetitions.append(petition)
+                }
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func clearFilter(alert: UIAlertAction!) {
+        filteredPetitions.removeAll(keepingCapacity: false)
+        filteredPetitions = allPetitions
+        tableView.reloadData()
     }
 
 }
