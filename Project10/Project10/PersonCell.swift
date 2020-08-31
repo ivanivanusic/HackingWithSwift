84a11a92a5c841afdfa31317f4eaa6caf5f0fa61
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-08-31 13:06:42 +0200

    Project 10, Day 1

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ivan Ivanušić on 31/08/2020.
+//  Copyright © 2020 Ivan Ivanušić. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Ivan Ivanušić on 31/08/2020.
+//  Copyright © 2020 Ivan Ivanušić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        
+        return cell
+    }
+
+
+}
+
