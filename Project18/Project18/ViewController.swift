12dffbd52d84b0d0891d662ae85677ddf1db17ed
Ivan Ivanusic <ivan.ivanusic@hotmail.com>
2020-09-17 20:41:34 +0200

    Project 18, Day 1

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Ivan Ivanušić on 17/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number: \(i)")
+        }
+    }
+
+
+}
+
