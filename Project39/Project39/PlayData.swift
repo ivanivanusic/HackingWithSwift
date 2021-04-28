f77ca2e7e5c4036c7a812024045def8013b26291
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-04-28 15:40:12 +0200

    Project 39 - Done

---
+++ Project39/Project39/PlayData.swift
@@ -0,0 +1,40 @@
+//
+//  PlayData.swift
+//  Project39
+//
+//  Created by Ivan Ivanušić on 28.04.2021..
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+    var wordCounts: NSCountedSet!
+    private(set) var filteredWords = [String]()
+
+    init() {
+        if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
+            if let plays = try? String(contentsOfFile: path) {
+                allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
+                allWords = allWords.filter { $0 != "" }
+                
+                wordCounts = NSCountedSet(array: allWords)
+                let sorted = wordCounts.allObjects.sorted { wordCounts.count(for: $0) > wordCounts.count(for: $1) }
+                allWords = sorted as! [String]
+            }
+        }
+        applyUserFilter("swift")
+    }
+    
+    func applyFilter(_ filter: (String) -> Bool) {
+        filteredWords = allWords.filter(filter)
+    }
+    
+    func applyUserFilter(_ input: String) {
+        if let userNumber = Int(input) {
+            applyFilter { self.wordCounts.count(for: $0) >= userNumber }
+        } else {
+            applyFilter { $0.range(of: input, options: .caseInsensitive) != nil }
+        }
+    }
+}
